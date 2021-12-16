4c976f42a486e5bc3843a967941246162f3d17dd
TheDro <andrewsc32@protonmail.com>
2021-12-16 16:48:11 -0500

    Day 16 part 1 complete

--- src/days/day16.vue
+++ src/days/day16.vue
@@ -2,55 +2,96 @@
 import {watchEffect, reactive} from 'vue'
 import _ from 'lodash'
 import {imagesc, expose} from "../helpers/helper.js";
-import {testData, data} from './day16.data.js'
+import {testData, data as realData} from './day16.data.js'
 import * as math from 'mathjs'
 
 expose({math, testData, _})
 
 export default {
   setup(){
-    let data = '38006F45291200'
+    let data = ''
+    data = 'D2FE28'
+    data = '38006F45291200'
+    data = 'EE00D40C823060'
+    data = '8A004A801A8002F478'
+    data = '620080001611562C8802118E34'
+    data = 'C0015000016115A2E0802F182340'
+    data = 'A0016C880162017C3686B18A3D4780'
+    data = realData
 
     let message = {hex: data.split(''), bin: []}
-    let packets = []
-    function parsePackets(message) {
-      let version = parseInt(getBits(message, 3).join(''), 2)
-      let typeId = parseInt(getBits(message, 3).join(''), 2)
+    // let packets = []
+    function parsePackets(message, number) {
+      let packets = []
+      for (let i=0; i<number; i++) {
+        if (message.bin.length === 0 && message.hex.length === 0) {
+          break
+        }
+        let messageStr = JSON.stringify({hex: message.hex.join(''), bin: message.bin.join('')})
+        let version = parseInt(getBits(message, 3).join(''), 2)
+        let typeId = parseInt(getBits(message, 3).join(''), 2)
+        console.log({version, typeId, messageStr})
+        let packet = {version, typeId}
 
-      if (typeId === 4) {
-        let value = []
-        for(let n=0; n<10; n++) {
-          let leadingBit = getBits(message,1)[0]
-          value.push(...getBits(message,4))
-          if (leadingBit === '0') {
-            break
+        if (typeId === 4) {
+
+          let value = []
+          for (let n = 0; n < 99; n++) {
+            let leadingBit = getBits(message, 1)[0]
+            value.push(...getBits(message, 4))
+            if (leadingBit === '0') {
+              break
+            }
+
           }
-
-        }
-        packets.push({version, typeId, value: parseInt(value.join(''), 2)})
-      } else {
-        let lengthTypeId = getBits(message, 1)[0]
-        // TODO: I'm here
-        if (lengthTypeId === '0') {
-          let bitLength = parseInt(getBits(message, 15).join(''), 2)
-          parseBits(message, bitLength)
+          packet.value = parseInt(value.join(''), 2)
+          console.log(`getting raw value: ${packet.value}`)
+          packets.push(packet)
+          // message.bin = []
         } else {
-          let packetLength = parseInt(getBits(message, 11).join(''), 2)
-          parsePackets(message, packetLength)
+          let lengthTypeId = getBits(message, 1)[0]
+          if (lengthTypeId === '0') {
+            let bitLength = parseInt(getBits(message, 15).join(''), 2)
+
+            let subMessage = {hex: [], bin: getBits(message, bitLength)}
+            console.log(`getting packets from next ${bitLength} bits: ${subMessage.bin.join('')}`)
+            packet.subPackets = parsePackets(subMessage, 99)
+            packets.push(packet)
+          } else {
+            let packetLength = parseInt(getBits(message, 11).join(''), 2)
+            console.log(`getting next ${packetLength} packets from ${JSON.stringify(message)}`)
+            packet.subPackets = parsePackets(message, packetLength)
+            packets.push(packet)
+          }
         }
+
       }
-
-
-      // let header = getHeader(hex)
-      console.log({version, typeId, packets})
+      console.log({packets})
+      return packets
     }
 
-    parsePackets(message)
+    let result = parsePackets(message, 1)[0]
+    console.log({result})
+    console.log({sum: versionSum([result])})
+
+
+    function versionSum(packets) {
+      let sum = 0
+      packets.forEach((packet) => {
+        sum += packet.version
+        if (packet.subPackets) {
+          sum += versionSum(packet.subPackets)
+        }
+      })
+      return sum
+    }
 
     function getBits(message, n) {
       if (message.bin.length < n) {
         let neededDigits = Math.ceil((n-message.bin.length)/4)
-        message.bin.push( ...hexToBinary(message.hex.splice(0, neededDigits)) )
+        for (let i=0; i<neededDigits; i++) {
+          message.bin.push( ...hexToBinary(message.hex.splice(0, 1)) )
+        }
       }
       return message.bin.splice(0,n)
     }
@@ -66,7 +107,7 @@ export default {
 
 
 
-    expose({data, hexToBinary, message, getBits})
+    expose({data, hexToBinary, message, getBits, result})
 
     return {}
   }

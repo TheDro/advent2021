68d03f789c7e3d032e8fdcfa7e9b98f42e8b1ae5
TheDro <andrewsc32@protonmail.com>
2021-12-21 07:35:33 -0500

    Completed day 21 part 1

--- src/components/Advent.vue
+++ src/components/Advent.vue
@@ -1,6 +1,6 @@
 <script>
 import {reactive} from 'vue'
-import Day from '../days/day20.vue'
+import Day from '../days/day21.vue'
 
 export default {
   components: {Day}
--- src/days/day21.vue
+++ src/days/day21.vue
@@ -9,11 +9,52 @@ let realData = {player1: 7, player2: 10}
 let testData = {player1: 4, player2: 8}
 expose({math, testData, _})
 
+function Die(startingValue) {
+  let value = startingValue
+  let maxValue = 100
+  let self = {value, roll, totalRolls: 0}
+  return self
+
+  function roll(n) {
+    let result = 0
+    for (let i=0; i<n; i++) {
+      result += self.value
+      self.value = self.value % maxValue + 1
+    }
+    self.totalRolls += n
+    return result
+  }
+}
+
+
 export default {
   setup(){
     let data = testData
+    data = realData
 
-    expose({data})
+    let die = Die(1)
+
+    let players = [
+      {position: data.player1-1, score: 0},
+      {position: data.player2-1, score: 0}
+    ]
+
+
+    for (let n=0; n<1000; n++) {
+      let playerIndex = n % 2
+      let player = players[playerIndex]
+      let roll = die.roll(3)
+      player.position = (player.position+roll) % 10
+      player.score += player.position+1
+      if (player.score >= 1000) {
+        break
+      }
+    }
+    console.log({players, die})
+    console.log({result1: die.totalRolls*math.min(players[0].score, players[1].score)})
+
+
+    expose({data, die, Die})
 
     return {}
   }

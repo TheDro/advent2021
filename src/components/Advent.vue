cde0d8932d75eefeffcf51a2c7a277980222c051
TheDro <andrewsc32@protonmail.com>
2021-12-22 06:18:15 -0500

    Starting day 22

--- src/components/Advent.vue
+++ src/components/Advent.vue
@@ -1,6 +1,6 @@
 <script>
 import {reactive} from 'vue'
-import Day from '../days/day21.vue'
+import Day from '../days/day22.vue'
 
 export default {
   components: {Day}
---
+++ src/days/day22.vue
@@ -0,0 +1,30 @@
+<script>
+import {watchEffect, reactive} from 'vue'
+import _ from 'lodash'
+import {imagesc, expose} from "../helpers/helper.js";
+import {testData, data} from './day22.data.js'
+import * as math from 'mathjs'
+
+expose({math, testData, _})
+
+export default {
+  setup(){
+    let data = testData
+
+    expose({data})
+
+    return {}
+  }
+}
+
+</script>
+
+<template>
+  <!--  <div v-for="line of basins">-->
+  <!--    <code>{{line}}</code>-->
+  <!--  </div>-->
+</template>
+
+<style>
+
+</style>
